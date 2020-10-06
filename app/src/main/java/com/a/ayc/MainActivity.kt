26768cf7ac6e6257df2bc06e2c978cd0f6a027cc
AmirHossein Ghafoorian <amirhgh1377@gmail.com>
2020-10-06 10:43:49 +0330

    configuration almost Done .

--- app/src/main/java/com/a/ayc/MainActivity.kt
+++ app/src/main/java/com/a/ayc/MainActivity.kt
@@ -1,24 +1,12 @@
 package com.a.ayc
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val create = FirebaseAuth.getInstance()
-
-        create.createUserWithEmailAndPassword("amir@gmail.com", "12345678")
-            .addOnCompleteListener { task ->
-                val a = task
-                if (task.isSuccessful) {
-                    Toast.makeText(this, "acc created successfully", Toast.LENGTH_SHORT).show()
-                }
-            }
     }
 }
