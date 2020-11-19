7b6ea14b696471d9ed8bb9cbcfb15f3c80eed43c
amirhosseinghafourian <amirhossein.ghafoorian@gmail.com>
2020-11-19 11:10:19 +0330

    Sign up with firebase : 1.3

--- app/src/main/java/com/a/ayc/user/ui/SignUpFragment.kt
+++ app/src/main/java/com/a/ayc/user/ui/SignUpFragment.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.a.ayc.R
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_sign_up.*
+
 
 @AndroidEntryPoint
 class SignUpFragment : Fragment() {
 
     private val signUpViewModel: SignUpViewModel by viewModels()
+    lateinit var mAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,13 +29,36 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mAuth = FirebaseAuth.getInstance()
+
         btn_sign_up.setOnClickListener {
-            validateInputs()
+            val email = signUp_et_2.editText?.text.toString()
+            val password = signUp_et_3.editText?.text.toString()
+            if (validateInputs(email, password)) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                requireContext(),
+                                "Signed Up Successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                "Sign Up Failed",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
         }
     }
 
-    private fun validateInputs() {
-        signUpViewModel.validateEmail(signUp_et_2.editText?.text.toString())
-        signUpViewModel.validatePassword(signUp_et_3.editText?.text.toString())
-    }
+    private fun validateInputs(
+        email: String,
+        password: String
+    ) =
+        signUpViewModel.validateEmail(email) &&
+                signUpViewModel.validatePassword(password)
 }
