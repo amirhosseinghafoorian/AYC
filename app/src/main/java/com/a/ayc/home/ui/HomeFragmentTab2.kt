e06b9ee1df72ec5f5a0e3a1c7e8c5a7077b0e8b7
amirhosseinghafourian <amirhossein.ghafoorian@gmail.com>
2020-11-22 17:18:14 +0330

    working on search users

--- app/src/main/java/com/a/ayc/home/ui/HomeFragmentTab2.kt
+++ app/src/main/java/com/a/ayc/home/ui/HomeFragmentTab2.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.a.ayc.R
+import kotlinx.android.synthetic.main.fragment_home_tab2.*
 
 class HomeFragmentTab2 : Fragment() {
 
@@ -14,6 +15,21 @@ class HomeFragmentTab2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home_tab2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val users = mutableListOf<String>()
+        repeat(100){
+            users.add("user : $it")
+        }
+
+        val myAdapter = UserListAdapter(users)
+
+        home_page_user_list_recycler.apply {
+            adapter = myAdapter
+        }
     }
 
 }
