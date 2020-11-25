36cde5f0aaea8f1698382ef9486ff73a58155fcd
amirhosseinghafourian <amirhossein.ghafoorian@gmail.com>
2020-11-25 16:33:00 +0330

    working on chat design

--- app/src/main/java/com/a/ayc/chat/ui/ChatFragment.kt
+++ app/src/main/java/com/a/ayc/chat/ui/ChatFragment.kt
@@ -8,6 +8,7 @@ import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.a.ayc.R
 import com.a.ayc.databinding.FragmentChatBinding
@@ -16,9 +17,6 @@ import com.a.ayc.general.MessageType
 import com.a.ayc.model.MessageModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_chat.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -64,73 +62,71 @@ class ChatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        chatViewModel.receiverUsername.observe(viewLifecycleOwner, { name ->
-            if (name != null) {
-                binding.username = name
-            }
-        })
+        lifecycleScope.launch {
 
-        chatViewModel.isInDirect.observe(viewLifecycleOwner, {
-            if (it != null) {
-                val chatId = chatViewModel.chatIdDecide(messageReceiver)
-                if (it) {
-                    // open existing chat
-                } else {
-                    chatViewModel.createChatRoom(chatId)
-                    chatViewModel.putChatInDirect(messageReceiver, messageSender)
-                    chatViewModel.putChatInDirect(messageSender, messageReceiver)
+            chatViewModel.receiverUsername.observe(viewLifecycleOwner, { name ->
+                if (name != null) {
+                    binding.username = name
+                }
+            })
+
+            chatViewModel.isInDirect.observe(viewLifecycleOwner, { isInDirect ->
+                if (isInDirect != null) {
+                    val chatId = chatViewModel.chatIdDecide(messageReceiver)
+                    if (isInDirect) {
+                        // open existing chat
+                    } else {
+                        chatViewModel.createChatRoom(chatId)
+                        chatViewModel.putChatInDirect(messageReceiver, messageSender)
+                        chatViewModel.putChatInDirect(messageSender, messageReceiver)
+                    }
+                }
+            })
+
+            chatViewModel.usernameFromUid(messageReceiver)
+            chatViewModel.isUserInDirect(
+                chatViewModel.currentUser()?.uid.toString(),
+                messageReceiver
+            )
+
+            val list = mutableListOf<MessageModel>()
+
+            myAdapter = ChatAdapter(list)
+
+            chat_recycler.apply {
+                adapter = myAdapter
+            }
+
+            send_cv.setOnClickListener {
+
+                if (chat_type_et.editText?.text?.isNotBlank() == true) {
+
+                    list.add(
+                        MessageModel(
+                            "",
+                            chat_type_et.editText?.text.toString(),
+                            MessageType.SENT
+                        )
+                    )
+
+                    // here it should check that if it is a sent message gone the seen icon
+                    // and if it is a received message visible the icon
+
+                    chat_type_et.editText?.setText("")
+
+                    myAdapter.list = list
+                    myAdapter.notifyItemInserted(list.size)
+
+                    chat_recycler.scrollToPosition(list.size - 1)
                 }
             }
-        })
-
-        chatViewModel.usernameFromUid(messageReceiver)
-        chatViewModel.isUserInDirect(chatViewModel.currentUser()?.uid.toString(), messageReceiver)
-
-        val list = mutableListOf<MessageModel>()
-
-        myAdapter = ChatAdapter(list)
-
-        chat_recycler.apply {
-            adapter = myAdapter
-        }
-
-        CoroutineScope(Dispatchers.Main).launch {
-
-            repeat(20) {
-
-                list.add(
-                    MessageModel(
-                        it.toString(),
-                        "message : $it",
-                        MessageType.RECEIVED
-                    )
-                )
-
-                myAdapter.list = list
-                myAdapter.notifyItemInserted(list.size)
-
-                delay(500)
-
-                chat_recycler.scrollToPosition(list.size)  // we have crash here
-
-                list.add(
-                    MessageModel(
-                        it.toString(),
-                        "message : $it",
-                        MessageType.SENT
-                    )
-                )
-
-                myAdapter.list = list
-                myAdapter.notifyItemInserted(list.size)
-
-                delay(500)
-
-                chat_recycler.scrollToPosition(list.size)
-            }
 
         }
 
+    }
 
+    override fun onPause() {
+        super.onPause()
+        // disable seen icon
     }
 }
